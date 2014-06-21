0e1f330c0a47f2db9522c8f7ed1965b2fd94e5ef
LFDM <1986gh@gmail.com>
2014-06-21 14:44:55 +0200

    Add app

---
+++ app.rb
@@ -0,0 +1,83 @@
+#!/usr/bin/env ruby
+
+require 'nokogiri'
+
+class SectionList
+  def initialize
+    @sections = []
+    parse_files
+  end
+
+  def parse_files
+    Dir.chdir('data') do
+      Dir.glob('*').each do |file|
+        parse_file(file)
+      end
+    end
+    sort_sections
+  end
+
+  def sort_sections
+    @sections.sort!
+  end
+
+  def parse_file(file)
+    doc = Nokogiri::HTML(File.read(file))
+    doc.css('.smythp').each do |section|
+      add_section(section, file)
+    end
+  end
+
+  def to_csv
+    @sections.map(&:to_csv).join("\n")
+  end
+
+  def to_json
+    "{\n#{@sections.map { |section| "  #{section.to_json}" }.join(",\n")}\n}"
+  end
+
+  def add_section(section, file)
+    @sections << Section.new(section, file)
+  end
+end
+
+class Section
+  attr_reader :section, :file, :id
+
+  include Comparable
+
+  def initialize(section, file)
+    @section = section
+    @file = file
+    @id = @section.attr('id')
+  end
+
+  def id_value
+    @id[1..-1].to_i
+  end
+
+  def <=>(other)
+    if is_section? && other.is_section?
+      id_value <=> other.id_value
+    else
+      @id <=> other.id
+    end
+  end
+
+  def is_section?
+    @id.start_with?('s')
+  end
+
+  def to_csv
+    "#{@id},#{@file}"
+  end
+
+  def to_json
+    %{"#{@id}" : "#{@file}"}
+  end
+end
+
+method = "to_#{ARGV.shift}"
+puts SectionList.new.send(method)
+
+
